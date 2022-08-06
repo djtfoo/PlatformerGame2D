c6b8c46f8bae921917c2181ca331186f69c3feb2
JT <djt.foo@gmail.com>
2022-08-06 14:00:24 +0800

    Empty Unity project with assets

---
+++ Assets/Scripts/MapLoader.cs
@@ -0,0 +1,28 @@
+/**
+ * Created: 6 Aug 2022
+ * 
+ * Class: MapLoader
+ * Generate the map of the level from a text file.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLoader : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset levelData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
